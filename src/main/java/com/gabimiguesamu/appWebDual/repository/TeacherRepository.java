2ecafd282ad8cf18cb1104b6e3b5ace4cb6a64ca
gabri <gabrielrl2004@gmail.com>
2024-01-08 14:01:32 +0100

    Repositorio de Teacher creado

---
+++ src/main/java/com/gabimiguesamu/appWebDual/repository/TeacherRepository.java
@@ -0,0 +1,7 @@
+package com.gabimiguesamu.appWebDual.repository;
+
+import com.gabimiguesamu.appWebDual.model.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+}
