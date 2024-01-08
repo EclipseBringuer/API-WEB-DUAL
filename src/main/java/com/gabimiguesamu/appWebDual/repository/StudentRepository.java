285f6fee801baaa30ef3e3fa89944c4538cc12eb
gabri <gabrielrl2004@gmail.com>
2024-01-08 15:35:40 +0100

    Repositorio de Student creado

---
+++ src/main/java/com/gabimiguesamu/appWebDual/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.gabimiguesamu.appWebDual.repository;
+
+import com.gabimiguesamu.appWebDual.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, String> {
+}
