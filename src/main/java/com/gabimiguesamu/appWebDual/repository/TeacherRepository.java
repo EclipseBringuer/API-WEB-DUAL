94262a8179011642680e6be06ca143b6e4d466cc
gabri <gabrielrl2004@gmail.com>
2024-01-08 14:02:18 +0100

    Indicacion del repositorio a Spring

--- src/main/java/com/gabimiguesamu/appWebDual/repository/TeacherRepository.java
+++ src/main/java/com/gabimiguesamu/appWebDual/repository/TeacherRepository.java
@@ -2,6 +2,8 @@ package com.gabimiguesamu.appWebDual.repository;
 
 import com.gabimiguesamu.appWebDual.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
 }
