35d0f1f055acf4649879fab731310b6cc8650cd9
gabri <gabrielrl2004@gmail.com>
2024-01-08 13:13:55 +0100

    Commit incial

---
+++ src/test/java/com/gabimiguesamu/appWebDual/AppWebDualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabimiguesamu.appWebDual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebDualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
