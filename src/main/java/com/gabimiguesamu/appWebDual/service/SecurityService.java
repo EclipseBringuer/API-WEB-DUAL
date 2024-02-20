bffa3b4a5a7427e478f00a5ae4a38b6f7b1f0fc4
ElUltimoPasajero <elultimopasajero87@gmail.com>
2024-02-20 18:27:15 +0100

    añadido SecurityService

---
+++ src/main/java/com/gabimiguesamu/appWebDual/service/SecurityService.java
@@ -0,0 +1,21 @@
+package com.gabimiguesamu.appWebDual.service;
+import com.gabimiguesamu.appWebDual.model.Student;
+import com.gabimiguesamu.appWebDual.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+/**Metodo para validar el email y la contraseña del usuario**/
+@Service
+public class SecurityService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public Student validateEmailAndPassword(String email, String password) {
+        Student student = studentRepository.findByEmailAndPassword(email);
+
+        // Devolver el objeto Student si las credenciales son válidas, de lo contrario, devolver null
+        return (student != null && student.getPassword().equals(password)) ? student : null;
+    }
+}
