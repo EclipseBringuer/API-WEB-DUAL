2886c6675d23091a037d813e726cb4e8b977a0f8
gabri <gabrielrl2004@gmail.com>
2024-02-21 08:29:24 +0100

    Lanzamiento de la web en el arranque de la API en mac y windows

--- src/main/java/com/gabimiguesamu/appWebDual/AppWebDualApplication.java
+++ src/main/java/com/gabimiguesamu/appWebDual/AppWebDualApplication.java
@@ -1,5 +1,6 @@
 package com.gabimiguesamu.appWebDual;
 
+import com.gabimiguesamu.appWebDual.util.Utility;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class AppWebDualApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppWebDualApplication.class, args);
+		Utility.launchWeb();
 	}
 
 }
---
+++ src/main/java/com/gabimiguesamu/appWebDual/util/Utility.java
@@ -0,0 +1,18 @@
+package com.gabimiguesamu.appWebDual.util;
+
+import java.io.IOException;
+
+public class Utility {
+    public static void launchWeb() {
+        String os = System.getProperty("os.name").toLowerCase();
+        try {
+            if(os.contains("win")) {
+                Runtime.getRuntime().exec("cmd /c start chrome http://localhost:8080/web/login");
+            }else{
+                Runtime.getRuntime().exec("google-chrome http://localhost:8080/web/login");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
