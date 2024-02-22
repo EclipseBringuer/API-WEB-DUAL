3d8e38f1737ecc19336545d1f84547f112587055
ElUltimoPasajero <elultimopasajero87@gmail.com>
2024-02-22 18:15:07 +0100

    TH de los html

--- src/main/java/com/gabimiguesamu/appWebDual/controller/EnterpriseController.java
+++ src/main/java/com/gabimiguesamu/appWebDual/controller/EnterpriseController.java
@@ -1,8 +1,11 @@
 package com.gabimiguesamu.appWebDual.controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/enterprise")
 public class EnterpriseController {
 
     @GetMapping("/enterprise/goBack")
@@ -10,4 +13,11 @@ public class EnterpriseController {
 
         return "index";
     }
+    @GetMapping("/preview")
+    public String previewEnterprise(Model model) {
+
+
+        return "enterprise";
+    }
+
 }
