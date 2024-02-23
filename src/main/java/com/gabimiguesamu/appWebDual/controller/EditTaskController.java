1da690cbeac222ad31eee45198ea6253336d706a
gabri <gabrielrl2004@gmail.com>
2024-02-23 19:20:31 +0100

    enlaces tabla

--- src/main/java/com/gabimiguesamu/appWebDual/controller/EditTaskController.java
+++ src/main/java/com/gabimiguesamu/appWebDual/controller/EditTaskController.java
@@ -6,11 +6,13 @@ import com.gabimiguesamu.appWebDual.service.ExtraCurricularServiceImpl;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 
 @Controller
 @RequestMapping("")
@@ -24,36 +26,48 @@ public class EditTaskController {
     }
 
     @GetMapping("/taskEdit")
-    public String landPage(){
+    public String landPage() {
         return "editAndShow";
     }
+
     @GetMapping("/backwards")
-    public String backwards(){
+    public String backwards() {
         return "index";
     }
 
+    @GetMapping("/taskEdit/{id}")
+    public String loadToUpdate(@PathVariable Long id, HttpSession session, Model model) {
+        Student s = (Student) session.getAttribute("alumno");
+        for (Task task: s.getTaskList()) {
+            if(Objects.equals(task.getId(), id)){
+               model.addAttribute("actividad", task);
+            }
+        }
+        return "editAndShow";
+    }
+
     @PostMapping("/save")
-    public String saveActivity(@ModelAttribute Task t, HttpSession session){
+    public String saveActivity(@ModelAttribute Task t, HttpSession session) {
 
         Student s = (Student) session.getAttribute("alumno");
 
         s.getTaskList().add(t);
 
-        session.setAttribute("alumno",s);
+        session.setAttribute("alumno", s);
 
         return "index";
 
     }
 
     @PostMapping("/update")
-    public String updateActivity(@ModelAttribute Task t, HttpSession session){
+    public String updateActivity(@ModelAttribute Task t, HttpSession session) {
 
-       Student s = (Student) session.getAttribute("alumno");
+        Student s = (Student) session.getAttribute("alumno");
 
-       s.getTaskList().add(t);
+        s.getTaskList().add(t);
 
-       //TODO comprobar que se le ponga al alumno en la base de datos
-       extraCurricularService.saveNewActivity(t);
+        //TODO comprobar que se le ponga al alumno en la base de datos
+        extraCurricularService.saveNewActivity(t);
 
         return "index";
 
@@ -72,7 +86,6 @@ public class EditTaskController {
 
         return "index";
     }
-
 
 
 }
